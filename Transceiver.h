7fd1def45b34c6da8c76c9626e8ee7660bf540df
Shamseddin Elmasri <61571666+shamseddinElmasri@users.noreply.github.com>
2020-10-27 00:24:22 -0400

    Update Transceiver.h

--- Transceiver.h
+++ Transceiver.h
@@ -23,8 +23,45 @@
 #define	CSN	GPIO_PIN_8
 #define	IRQ	GPIO_PIN_0
 
+#define	MYADDRESS	1
+#define PACKETLENGTH 32
+
 extern SPI_HandleTypeDef hspi2;
 
+
+struct packetHeader{
+	uint8_t destAddr;					// Destination address
+	uint8_t sourceAddr;					// Source address
+	uint8_t TTL;						// Time to live
+	uint8_t type;						// Packet type
+	uint8_t packetFlags;				// Ack, Last packet, ... more to be added
+	uint8_t PID;						// Packet ID
+	uint8_t checksum;
+};
+
+
+/*
+ * enum for node address
+ */
+enum nodeAddr{
+	NODE_1	= 1,
+	NODE_2,
+	NODE_3,
+	NODE_4,
+	NODE_5,
+	NODE_6,
+	MAX_NODE
+};
+
+/*
+ * enum for packet type
+ */
+enum packetType{
+	DATA = 0,
+	ADVERTISEMENT,
+	ACK,
+	PACKET_TYPE_MAX
+};
 
 /*
  * enum for data rate (made for nRF24L01+)
@@ -50,6 +87,11 @@ void CSN_HIGH(void);
 void CE_LOW(void);
 void CE_HIGH(void);
 void setDataRate(dataRate dRate);
+struct packetHeader disassemblePacket(uint8_t *receivedData, uint8_t *receivedPacket);
+void assemblePacket(char* payload, uint8_t* _packet);
+uint8_t countSetBits(uint8_t n);
+void displayHeader(const struct packetHeader *_packetHeader);
+void displayData(uint8_t *receivedData);
 
 uint8_t hal_nrf_rw(uint8_t value);
 
